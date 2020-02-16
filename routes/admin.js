ee9afdecec6a59e0082d15b529688ff4a9b1c9ac
sholabioku <bilushonline@gmail.com>
2020-02-16 18:45:39 +0100

    Add filter for routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
   // console.log('In another middleware');
-  res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add product</button></form>');
+  res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add product</button></form>');
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
