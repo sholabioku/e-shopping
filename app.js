ed373a8d41cd8ba514d377451fcffe05cb636720
sholabioku <bilushonline@gmail.com>
2020-02-16 17:41:48 +0100

    Pass incoming request

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({ extended: true }));
 
-server.listen(3000, console.log('app connected'));
+app.use('/', (req, res, next) => {
+  // console.log('this always run');
+  next();
+});
+
+app.use('/add-product', (req, res, next) => {
+  // console.log('In another middleware');
+  res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  // console.log('In another middleware');
+  res.send('<h1>Hello from express!</h1>');
+});
+
+app.listen(3000, console.log('app connected'));
