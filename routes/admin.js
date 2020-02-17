3a1266951745f3d99ba00ca387d45e18ea4c3e1c
sholabioku <bilushonline@gmail.com>
2020-02-17 04:08:50 +0100

    Serve html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/add-product', (req, res, next) => {
-  // console.log('In another middleware');
-  res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Add product</button></form>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/', (req, res, next) => {
-  // console.log('In another middleware');
-  res.send('<h1>Hello from express!</h1>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
