04327f3f939504790fccb079e8b5503cba79df91
sholabioku <bilushonline@gmail.com>
2020-02-18 04:31:01 +0100

    Share data across request users

--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const path = require('path');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
+  console.log('From shop: ', adminData.products);
+
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
