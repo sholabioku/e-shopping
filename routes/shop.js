3ef567459029ffcc0abb30553923feac401fe6b8
sholabioku <bilushonline@gmail.com>
2020-02-19 09:34:18 +0100

    Install and inplement pug template enginr

--- routes/shop.js
+++ routes/shop.js
@@ -6,9 +6,7 @@ const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-  console.log('From shop: ', adminData.products);
-
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
 });
 
 module.exports = router;
