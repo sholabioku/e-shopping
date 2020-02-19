2c4c396233f7c98c9511612b7f8b1b1320595250
sholabioku <bilushonline@gmail.com>
2020-02-19 21:39:23 +0100

    Implement express-handlebars for content output

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main', extname: 'hbs' }));
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
