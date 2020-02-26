e323cc821617dadb039b59402b285c1791501a5f
sholabioku <bilushonline@gmail.com>
2020-02-26 08:27:20 +0100

    Insert data to database

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,7 +3,7 @@ const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
-    .then(([rows, fieldData]) => {
+    .then(([rows]) => {
       res.render('shop/product-list', {
         prods: rows,
         pageTitle: 'All Products',
@@ -26,7 +26,7 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll()
-    .then(([rows, fieldData]) => {
+    .then(([rows]) => {
       res.render('shop/index', {
         prods: rows,
         pageTitle: 'Shop',
