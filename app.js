2e1b1d78865981c98de2bbef843fe02346d8ee1f
sholabioku <bilushonline@gmail.com>
2020-02-27 12:29:06 +0100

    Sync products model to database

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -21,4 +21,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000, console.log('app connected'));
+sequelize
+  .sync({ force: true })
+  .then(result => {
+    // console.log(result);
+    app.listen(3000, console.log('app connected'));
+  })
+  .catch(err => console.log(err));
