aa7125fd3035772dd453025ae85a26d1d413c3e4
sholabioku <bilushonline@gmail.com>
2020-02-27 14:11:40 +0100

    Fetch one product from database

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,11 +15,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findAll({ where: { id: prodId } })
-    .then(products => {
+  Product.findByPk(prodId)
+    .then(product => {
       res.render('shop/product-detail', {
-        product: products[0],
-        pageTitle: products[0].title,
+        product: product,
+        pageTitle: product.title,
         path: '/products',
       });
     })
