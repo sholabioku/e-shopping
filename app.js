0710d7ed3758cb2914edc96d4a23255edb884ab7
sholabioku <bilushonline@gmail.com>
2020-02-28 08:57:12 +0100

    Add middleware for managing the dummy user

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
@@ -17,6 +17,15 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoute);
 app.use(shopRoutes);
