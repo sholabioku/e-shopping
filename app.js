ee7039edb19f24f8b643f41e7aeba56ccf80d2e2
sholabioku <bilushonline@gmail.com>
2020-02-28 17:31:56 +0100

    Add fetching a cart

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -34,6 +36,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   .sync({ force: false })
@@ -48,6 +54,9 @@ sequelize
   })
   .then(user => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000, console.log('app connected'));
   })
   .catch(err => console.log(err));
