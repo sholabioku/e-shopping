ba92a155974a8940ec0f7588856569a8bf937553
sholabioku <bilushonline@gmail.com>
2020-03-02 17:00:14 +0100

    Create product in database

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,16 +13,11 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      imageUrl: imageUrl,
-      price: price,
-      description: description,
-      userId: req.user.id,
-    })
+  const product = new Product(title, price, imageUrl, description);
+  product
+    .save()
     .then(result => {
-      // console.log(result);
+      console.log(result);
       console.log('Product created');
       res.redirect('/admin/products');
     })
