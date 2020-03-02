f01104d3719978b95a413116744ec5f0d4fc478b
sholabioku <bilushonline@gmail.com>
2020-03-02 14:59:31 +0100

    Finish database connection

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
+const { mongoConnect } = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoute = require('./routes/admin');
+const adminRoute = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,12 +25,12 @@ app.use((req, res, next) => {
   //   .catch(err => console.log(err));
 });
 
-// app.use('/admin', adminRoute);
+app.use('/admin', adminRoute);
 // app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(client => {
-  console.log(client);
+mongoConnect(() => {
+  // console.log(client);
   app.listen(3000);
 });
--- util/database.js
+++ util/database.js
@@ -1,16 +1,30 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient } = require('mongodb');
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+
+let _db;
 
 const mongoConnect = cb => {
-  MongoClient.connect(
-    'mongodb://localhost:27017/NodeComplete',
-    { useUnifiedTopology: true }
-  )
+  MongoClient.connect('mongodb://localhost:27017/shop', {
+    useUnifiedTopology: true,
+  })
     .then(client => {
       console.log('Mongodb connected...');
-      cb(client);
+      _db = client.db();
+      cb();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
