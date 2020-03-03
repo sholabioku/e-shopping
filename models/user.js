a98bba14976bbc3d144310f565f5c5db0f7f6473
sholabioku <bilushonline@gmail.com>
2020-03-03 17:52:07 +0100

    Add get orders

--- models/user.js
+++ models/user.js
@@ -107,7 +107,13 @@ class User {
       });
   }
 
-  getOrders() {}
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .find({ 'user._id': new mongodb.ObjectId(this._id) })
+      .toArray();
+  }
 
   static findById(userId) {
     const db = getDb();
