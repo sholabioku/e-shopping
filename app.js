01260b35dec89ec9fe14edb43db29602fce97c70
sholabioku <bilushonline@gmail.com>
2020-03-04 08:10:39 +0100

    Connect to mongodb database with mongoose

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const { mongoConnect } = require('./util/database');
 const User = require('./models/user');
 
 const app = express();
@@ -31,7 +31,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  // console.log(client);
-  app.listen(3000);
-});
+mongoose
+  .connect('mongodb://localhost:27017/shop', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(3000, () => console.log('Database connected...'));
+  })
+  .catch(err => console.log(err));
