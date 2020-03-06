b830524975b630fed3a856eb37315f11f9ce6a87
sholabioku <bilushonline@gmail.com>
2020-03-06 12:16:47 +0100

    Add more configuration for cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,19 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req
-    .get('Cookie')
-    .split(';')[3]
-    .trim()
-    .split('=')[1];
+  // const isLoggedIn =
+  //   req
+  //     .get('Cookie')
+  //     .split(';')[3]
+  //     .trim()
+  //     .split('=')[1] === 'true';
 
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'isLoggedIn=true');
+  res.setHeader('Set-Cookie', 'isLoggedIn=true; HttpOnly ');
   res.redirect('/');
 };
