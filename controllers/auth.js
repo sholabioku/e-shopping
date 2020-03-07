e628fb8fa0a89b577af5b5de35bc4145e75b2dd8
sholabioku <bilushonline@gmail.com>
2020-03-07 04:15:36 +0100

    Delete cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,6 @@
-exports.getLogin = (req, res, next) => {
-  // const isLoggedIn =
-  //   req
-  //     .get('Cookie')
-  //     .split(';')[3]
-  //     .trim()
-  //     .split('=')[1] === 'true';
-  console.log(req.session.isLoggedIn);
+const User = require('../models/user');
 
+exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -15,6 +9,18 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect('/');
+  User.findById('5e5f8572ca9bba03a0db91b5')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
