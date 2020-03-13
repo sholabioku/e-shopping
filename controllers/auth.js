409b5ca02dcf7f5ca2a257b77174b7a2bfa5b167
sholabioku <bilushonline@gmail.com>
2020-03-13 08:17:57 +0100

    Add basic validation setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const crypto = require('crypto');
 const brcypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendGridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
@@ -77,6 +78,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array(),
+    });
+  }
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
